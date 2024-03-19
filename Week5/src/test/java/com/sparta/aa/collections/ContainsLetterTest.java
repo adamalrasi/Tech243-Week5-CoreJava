1ed807c5f25473e3b1993a0419914362cb885d39
Adam Alrasi <adam.alrasi1@gmail.com>
2024-03-19 15:13:09 +0000

    HashMap exercise

---
+++ Week5/src/test/java/com/sparta/aa/collections/ContainsLetterTest.java
@@ -0,0 +1,25 @@
+package com.sparta.aa.collections;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class ContainsLetterTest {
+    @Test
+    public void testConvertMessage() {
+        // Arrange
+        String expected = "thecatinthehatcomesback";
+        // Act
+        ContainsLetter container = new ContainsLetter();
+        String actual = container.convertMessage("The cat in the hat comes back");
+        // Assert
+        Assertions.assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testHashMapProgram() {
+        // Arrange
+        // Act
+        // Assert
+        
+    }
+}
